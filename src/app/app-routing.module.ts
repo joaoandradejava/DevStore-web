999c27c0f1318dfa4f64abb70d3d3f4f3fd79d9f
Vinicius Barros <vbarros.2001@hotmail.com>
2021-08-25 20:47:09 -0300

    teste2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'login', loadChildren: () => import('./view/login/login.module').then(m => m.LoginModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
